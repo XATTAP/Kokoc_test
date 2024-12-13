3f3f87531c9f79e1895ec5faff1675d8529d248a
XATTAP <cherednikovkirill@gmail.com>
2024-12-13 20:53:00 +0300

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from '#/src/config/configuration';
+import { TypeOrmModule } from '#/src/database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    TypeOrmModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/database/database.providers.ts
@@ -0,0 +1,63 @@
+import { DataSource } from 'typeorm';
+import configuration from '#/src/config/configuration';
+
+export const databaseProviders = {
+  development: [
+    {
+      provide: DataSource,
+      inject: [],
+      useFactory: async () => {
+        try {
+          const pg_config = configuration().pg_database;
+          const dataSource = new DataSource({
+            type: 'postgres',
+            host: pg_config.host,
+            port: pg_config.port,
+            username: pg_config.username,
+            password: pg_config.password,
+            database: pg_config.database_name,
+            entities: [__dirname + '/Entities/*.entity{.ts,.js}'],
+            synchronize: true,
+          });
+          await dataSource.initialize();
+          console.log(
+            `Database connected successfully on port ${dataSource.options['port']}`,
+          );
+          return dataSource;
+        } catch (error) {
+          console.log(`Error connecting to database\ncode: ${error.code}`);
+          throw error;
+        }
+      },
+    },
+  ],
+  test: [
+    {
+      provide: DataSource,
+      inject: [],
+      useFactory: async () => {
+        try {
+          const pg_config = configuration().pg_database;
+          const dataSource = new DataSource({
+            type: 'postgres',
+            host: pg_config.host,
+            port: pg_config.port,
+            username: pg_config.username,
+            password: pg_config.password,
+            database: pg_config.database_name,
+            entities: [__dirname + '/Entities/*.entity{.ts,.js}'],
+            synchronize: true,
+          });
+          await dataSource.initialize();
+          console.log(
+            `Test database connected successfully on port ${dataSource.options['port']}`,
+          );
+          return dataSource;
+        } catch (error) {
+          console.log(`Error connecting to test database\ncode: ${error.code}`);
+          throw error;
+        }
+      },
+    },
+  ],
+};
