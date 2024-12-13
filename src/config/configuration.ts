e1683f890b61f18bcea4a267df2585c891f84d8c
XATTAP <cherednikovkirill@gmail.com>
2024-12-13 20:00:21 +0300

    added config file

---
+++ src/config/configuration.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  port: parseInt(process.env.SERVER_PORT, 10) || 3000,
+  pg_database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.POSTGRES_USERNAME || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    database_name: process.env.POSTGRES_DATABASE || 'pg_db'
+  }
+})
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import configuration from '#/src/config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  await app.listen(configuration().port ?? 3000);
+  const url = await app.getUrl();
+  console.log(`server start on ${url}`);
 }
 bootstrap();
